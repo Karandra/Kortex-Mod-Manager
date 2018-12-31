a93ed00e6f0f195b88e8417bacf481e7bac0042b
KerberX <DaIvan97@gmail.com>
2018-12-31 09:13:28 +0300

    Added includes for C++ standard library

--- Kortex/Source/stdafx.h
+++ Kortex/Source/stdafx.h
@@ -11,6 +11,23 @@
 #pragma comment(lib, "Bin/KxFramework.lib")
 #endif
 
+// C++
+#include <string>
+#include <string_view>
+#include <vector>
+#include <array>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <functional>
+#include <variant>
+#include <optional>
+#include <locale>
+#include <limits>
+#include <type_traits>
+
 /* Kortex */
 #define KIPC_SERVER 0
 #include <Kortex/Core.hpp>

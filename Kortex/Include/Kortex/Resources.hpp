c8f32debf33542005066f4eb880b4b416b716e29
KerberX <DaIvan97@gmail.com>
2019-07-17 00:55:41 +0300

    Added resources header

---
+++ Kortex/Include/Kortex/Resources.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include "stdafx.h"
+
+#include "Application/Resources/IImageProvider.h"
+#include "Application/Resources/ResourceID.h"
+#include "Application/Resources/ImageResourceID.h"

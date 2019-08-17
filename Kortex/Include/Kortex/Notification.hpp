8945306a53730aa03fa7ab3527e1cc0cad07428c
KerberX <DaIvan97@gmail.com>
2019-08-17 03:47:25 +0300

    Removed redundant includes

--- Kortex/Include/Kortex/Notification.hpp
+++ Kortex/Include/Kortex/Notification.hpp
@@ -4,5 +4,3 @@
 #include "Application/Notifications/DefaultNotificationCenter.h"
 #include "Application/Notifications/BaseNotification.h"
 #include "Application/Notifications/SimpleNotification.h"
-#include "Application/Notifications/DisplayModel.h"
-#include "Application/Notifications/PopupWindow.h"

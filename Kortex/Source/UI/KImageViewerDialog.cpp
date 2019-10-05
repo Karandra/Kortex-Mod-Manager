adb84ed0c9835abb96f938e684676e31973ca6b5
Kerber <DaIvan97@gmail.com>
2019-10-06 02:21:38 +0300

    KImageViewerDialog moved into namespace

--- Kortex/Source/UI/KImageViewerDialog.cpp
+++ Kortex/Source/UI/KImageViewerDialog.cpp
@@ -3,14 +3,11 @@
 #include <Kortex/ApplicationOptions.hpp>
 #include <Kortex/ScreenshotsGallery.hpp>
 #include "KImageViewerDialog.h"
-#include "KMainWindow.h"
 #include "Utility/KAux.h"
+#include "Utility/UI.h"
 #include <Kortex/Application.hpp>
 #include <KxFramework/KxFileBrowseDialog.h>
 #include <KxFramework/KxString.h>
-
-using namespace Kortex;
-using namespace Kortex::Application;
 
 namespace Kortex::Application::OName
 {
@@ -20,291 +17,298 @@ namespace Kortex::Application::OName
 	KortexDefOption(ColorFG);
 }
 
-wxDEFINE_EVENT(KEVT_IMAGEVIEWER_NEXT_IMAGE, KImageViewerEvent);
-wxDEFINE_EVENT(KEVT_IMAGEVIEWER_PREV_IMAGE, KImageViewerEvent);
-
-bool KImageViewerEvent::HasBitmap() const
+namespace Kortex::UI
 {
-	return GetType() == BitmapPtr;
-}
-wxBitmap KImageViewerEvent::GetBitmap() const
-{
-	return HasBitmap() ? std::get<BitmapPtr>(m_Data) : wxNullBitmap;
-}
-void KImageViewerEvent::SetBitmap(const wxBitmap& bitmap)
-{
-	m_Data = bitmap.IsOk() ? bitmap : wxNullBitmap;
-}
-
-bool KImageViewerEvent::IsAnimationFile() const
-{
-	return HasFilePath() && Kortex::IScreenshotsGallery::IsAnimationFile(GetFilePath());
-}
-bool KImageViewerEvent::HasFilePath() const
-{
-	return GetType() == FilePath;
-}
-wxString KImageViewerEvent::GetFilePath() const
-{
-	return HasFilePath() ? std::get<FilePath>(m_Data) : KxNullWxString;
-}
-void KImageViewerEvent::SetFilePath(const wxString& filePath)
-{
-	m_Data = filePath;
-}
-
-bool KImageViewerEvent::IsInputStream() const
-{
-	return GetType() == InputStream;
-}
-wxMemoryInputStream KImageViewerEvent::GetInputSteram()
-{
-	if (IsInputStream())
+	bool KImageViewerEvent::HasBitmap() const
 	{
-		const KArchive::Buffer* buffer = std::get<InputStream>(m_Data);
-		if (buffer)
+		return GetType() == BitmapPtr;
+	}
+	wxBitmap KImageViewerEvent::GetBitmap() const
+	{
+		return HasBitmap() ? std::get<BitmapPtr>(m_Data) : wxNullBitmap;
+	}
+	void KImageViewerEvent::SetBitmap(const wxBitmap& bitmap)
+	{
+		m_Data = bitmap.IsOk() ? bitmap : wxNullBitmap;
+	}
+	
+	bool KImageViewerEvent::IsAnimationFile() const
+	{
+		return HasFilePath() && Kortex::IScreenshotsGallery::IsAnimationFile(GetFilePath());
+	}
+	bool KImageViewerEvent::HasFilePath() const
+	{
+		return GetType() == FilePath;
+	}
+	wxString KImageViewerEvent::GetFilePath() const
+	{
+		return HasFilePath() ? std::get<FilePath>(m_Data) : KxNullWxString;
+	}
+	void KImageViewerEvent::SetFilePath(const wxString& filePath)
+	{
+		m_Data = filePath;
+	}
+	
+	bool KImageViewerEvent::IsInputStream() const
+	{
+		return GetType() == InputStream;
+	}
+	wxMemoryInputStream KImageViewerEvent::GetInputSteram()
+	{
+		if (IsInputStream())
 		{
-			return wxMemoryInputStream(buffer->data(), buffer->size());
+			const KArchive::Buffer* buffer = std::get<InputStream>(m_Data);
+			if (buffer)
+			{
+				return wxMemoryInputStream(buffer->data(), buffer->size());
+			}
 		}
+		return wxMemoryInputStream(nullptr, 0);
 	}
-	return wxMemoryInputStream(nullptr, 0);
-}
-void KImageViewerEvent::SetInputStream(const KArchive::Buffer& buffer)
-{
-	m_Data = &buffer;
+	void KImageViewerEvent::SetInputStream(const KArchive::Buffer& buffer)
+	{
+		m_Data = &buffer;
+	}
 }
 
-//////////////////////////////////////////////////////////////////////////
-bool KImageViewerDialog::OnDynamicBind(wxDynamicEventTableEntry& entry)
+namespace Kortex::UI
 {
-	if (entry.m_eventType == KEVT_IMAGEVIEWER_NEXT_IMAGE)
+	bool KImageViewerDialog::OnDynamicBind(wxDynamicEventTableEntry& entry)
 	{
-		m_Forward->SetEnabled(true);
-	}
-	if (entry.m_eventType == KEVT_IMAGEVIEWER_PREV_IMAGE)
-	{
-		m_Backward->SetEnabled(true);
-	}
-	return KxStdDialog::OnDynamicBind(entry);
-}
-
-void KImageViewerDialog::OnLoadFromDisk(const wxString& filePath)
-{
-	if (Kortex::IScreenshotsGallery::IsAnimationFile(filePath))
-	{
-		m_ImageView->LoadFile(filePath);
-	}
-	else
-	{
-		m_ImageView->SetBitmap(wxBitmap(filePath, wxBITMAP_TYPE_ANY));
-	}
-}
-void KImageViewerDialog::OnNavigation(wxAuiToolBarEvent& event)
-{
-	KImageViewerEvent evt;
-	evt.SetEventObject(this);
-	if (event.GetEventObject() == m_Backward)
-	{
-		evt.SetEventType(KEVT_IMAGEVIEWER_PREV_IMAGE);
-	}
-	else
-	{
-		evt.SetEventType(KEVT_IMAGEVIEWER_NEXT_IMAGE);
-	}
-	HandleWindowEvent(evt);
-	OnAcceptNavigation(evt);
-}
-void KImageViewerDialog::OnAcceptNavigation(KImageViewerEvent& event)
-{
-	if (event.IsAllowed())
-	{
-		m_Backward->SetEnabled(event.HasPrev());
-		m_Forward->SetEnabled(event.HasNext());
-
-		wxString description = event.GetDescription();
-		if (!description.IsEmpty())
+		if (entry.m_eventType == KImageViewerEvent::EvtNext)
 		{
-			m_Splitter->Unsplit(m_ImageView);
-			m_Splitter->Unsplit(m_Description);
-
-			m_Description->SetValue(wxString::Format(wxS("<div align=\"center\">%s</div>"), description));
-			m_Description->Enable(true);
-
-			int nMinHeight = m_Splitter->GetMinimumPaneSize();
-			int height = -std::min(m_Description->GetTextExtent(description).GetHeight() + nMinHeight, nMinHeight);
-			m_Splitter->SplitHorizontally(m_ImageView, m_Description, height);
+			m_Forward->SetEnabled(true);
+		}
+		if (entry.m_eventType == KImageViewerEvent::EvtPrevious)
+		{
+			m_Backward->SetEnabled(true);
+		}
+		return KxStdDialog::OnDynamicBind(entry);
+	}
+	
+	void KImageViewerDialog::OnLoadFromDisk(const wxString& filePath)
+	{
+		if (Kortex::IScreenshotsGallery::IsAnimationFile(filePath))
+		{
+			m_ImageView->LoadFile(filePath);
 		}
 		else
 		{
-			m_Splitter->Unsplit(m_ImageView);
-			m_Splitter->Unsplit(m_Description);
-			m_Splitter->Initialize(m_ImageView);
-
-			m_Description->SetValue(KAux::MakeHTMLWindowPlaceholder(KTr("InstallWizard.NoDescriptionHint"), m_Description));
-			m_Description->Enable(false);
+			m_ImageView->SetBitmap(wxBitmap(filePath, wxBITMAP_TYPE_ANY));
 		}
-
-		if (event.HasBitmap())
+	}
+	void KImageViewerDialog::OnNavigation(wxAuiToolBarEvent& event)
+	{
+		KImageViewerEvent evt;
+		evt.SetEventObject(this);
+		if (event.GetEventObject() == m_Backward)
 		{
-			m_ImageView->SetBitmap(event.GetBitmap());
-			m_FilePath.Clear();
+			evt.SetEventType(KImageViewerEvent::EvtPrevious);
 		}
 		else
 		{
-			m_FilePath = event.GetFilePath();
-			OnLoadFromDisk(m_FilePath);
+			evt.SetEventType(KImageViewerEvent::EvtNext);
+		}
+		HandleWindowEvent(evt);
+		OnAcceptNavigation(evt);
+	}
+	void KImageViewerDialog::OnAcceptNavigation(KImageViewerEvent& event)
+	{
+		if (event.IsAllowed())
+		{
+			m_Backward->SetEnabled(event.HasPrev());
+			m_Forward->SetEnabled(event.HasNext());
+	
+			wxString description = event.GetDescription();
+			if (!description.IsEmpty())
+			{
+				m_Splitter->Unsplit(m_ImageView);
+				m_Splitter->Unsplit(m_Description);
+	
+				m_Description->SetValue(wxString::Format(wxS("<div align=\"center\">%s</div>"), description));
+				m_Description->Enable(true);
+	
+				int nMinHeight = m_Splitter->GetMinimumPaneSize();
+				int height = -std::min(m_Description->GetTextExtent(description).GetHeight() + nMinHeight, nMinHeight);
+				m_Splitter->SplitHorizontally(m_ImageView, m_Description, height);
+			}
+			else
+			{
+				m_Splitter->Unsplit(m_ImageView);
+				m_Splitter->Unsplit(m_Description);
+				m_Splitter->Initialize(m_ImageView);
+	
+				m_Description->SetValue(KAux::MakeHTMLWindowPlaceholder(KTr("InstallWizard.NoDescriptionHint"), m_Description));
+				m_Description->Enable(false);
+			}
+	
+			if (event.HasBitmap())
+			{
+				m_ImageView->SetBitmap(event.GetBitmap());
+				m_FilePath.Clear();
+			}
+			else
+			{
+				m_FilePath = event.GetFilePath();
+				OnLoadFromDisk(m_FilePath);
+			}
+		}
+		else
+		{
+			wxBell();
 		}
 	}
-	else
+	void KImageViewerDialog::OnScaleChanged(wxCommandEvent& event)
 	{
-		wxBell();
+		m_ImageView->SetScaleFactor(event.GetInt() / 100.0);
 	}
-}
-void KImageViewerDialog::OnScaleChanged(wxCommandEvent& event)
-{
-	m_ImageView->SetScaleFactor(event.GetInt() / 100.0);
-}
-void KImageViewerDialog::OnSaveImage(wxCommandEvent& event)
-{
-	KxFileBrowseDialog dialog(this, KxID_NONE, KxFBD_SAVE);
-	const KxStringVector& exts = Kortex::IScreenshotsGallery::GetSupportedExtensions();
-	const Kortex::ScreenshotsGallery::SupportedTypesVector& formats = Kortex::IScreenshotsGallery::GetSupportedFormats();
-	for (const wxString& ext: exts)
+	void KImageViewerDialog::OnSaveImage(wxCommandEvent& event)
 	{
-		dialog.AddFilter(ext, wxString::Format("%s %s", KTr("FileFilter.Image"), ext.AfterFirst('.').MakeUpper()));
-	}
-	dialog.SetDefaultExtension(exts[0]);
-	dialog.SetFileName(m_FilePath.AfterLast('\\').BeforeLast('.'));
-
-	if (dialog.ShowModal() == KxID_OK)
-	{
-		m_ImageView->GetBitmap().SaveFile(dialog.GetResult(), formats[dialog.GetSelectedFilter()]);
-	}
-}
-void KImageViewerDialog::OnChangeColor(wxColourPickerEvent& event)
-{
-	wxColour color = event.GetColour();
-	if (event.GetEventObject() == m_ColorBGCtrl)
-	{
-		m_ImageView->SetBackgroundColour(color);
-	}
-	else
-	{
-		m_ImageView->SetForegroundColour(color);
-	}
-	m_ImageView->Refresh();
-}
-
-bool KImageViewerDialog::Create(wxWindow* parent, const wxString& caption)
-{
-	if (KxStdDialog::Create(parent, KxID_NONE, KAux::StrOr(caption, KTr("ImageViewer.Caption")), wxDefaultPosition, wxDefaultSize, KxBTN_CLOSE))
-	{
-		AddButton(KxID_SAVE, wxEmptyString, true).GetControl()->Bind(wxEVT_BUTTON, &KImageViewerDialog::OnSaveImage, this);
-
-		SetMainIcon(KxICON_NONE);
-		SetWindowResizeSide(wxBOTH);
-		SetInitialSize(parent->GetSize().Scale(0.7f, 0.7f));
-
-		// Splitter
-		m_Splitter = new KxSplitterWindow(m_ContentPanel, KxID_NONE);
-		IThemeManager::GetActive().ProcessWindow(m_Splitter);
-		PostCreate(wxDefaultPosition);
-
-		// View
-		m_ImageView = new KxImageView(m_Splitter, KxID_NONE);
-		m_ImageView->SetScaleMode(KxIV_SCALE_ASPECT_FIT);
-		m_ImageView->SetBackgroundMode(KxIV_BG_TRANSPARENCY_PATTERN);
-		m_ImageView->Bind(wxEVT_RIGHT_UP, [this](wxMouseEvent& event)
+		KxFileBrowseDialog dialog(this, KxID_NONE, KxFBD_SAVE);
+		const KxStringVector& exts = Kortex::IScreenshotsGallery::GetSupportedExtensions();
+		const Kortex::ScreenshotsGallery::SupportedTypesVector& formats = Kortex::IScreenshotsGallery::GetSupportedFormats();
+		for (const wxString& ext: exts)
 		{
-			Close();
-		});
-		IThemeManager::GetActive().ProcessWindow(m_ImageView);
+			dialog.AddFilter(ext, wxString::Format("%s %s", KTr("FileFilter.Image"), ext.AfterFirst('.').MakeUpper()));
+		}
+		dialog.SetDefaultExtension(exts[0]);
+		dialog.SetFileName(m_FilePath.AfterLast('\\').BeforeLast('.'));
+	
+		if (dialog.ShowModal() == KxID_OK)
+		{
+			m_ImageView->GetBitmap().SaveFile(dialog.GetResult(), formats[dialog.GetSelectedFilter()]);
+		}
+	}
+	void KImageViewerDialog::OnChangeColor(wxColourPickerEvent& event)
+	{
+		wxColour color = event.GetColour();
+		if (event.GetEventObject() == m_ColorBGCtrl)
+		{
+			m_ImageView->SetBackgroundColour(color);
+		}
+		else
+		{
+			m_ImageView->SetForegroundColour(color);
+		}
+		m_ImageView->Refresh();
+	}
+	
+	KImageViewerDialog::KImageViewerDialog(wxWindow* parent, const wxString& caption)
+	{
+		if (Create(parent, caption))
+		{
+			SetSize(IMainWindow::GetDialogBestSize(this));
+			GetButton(KxID_CLOSE).GetControl()->SetFocus();
+			CenterOnScreen();
+		}
+	}
+	KImageViewerDialog::~KImageViewerDialog()
+	{
+		using namespace Application;
 
-		// Options
 		auto options = IApplication::GetInstance()->GetGlobalOption(OName::ImageViewer);
-		int64_t colorBG = options.GetAttributeInt(OName::ColorBG, -1);
-		if (colorBG != -1)
-		{
-			m_ImageView->SetBackgroundColour(KxColor::FromCOLORREF(colorBG));
-		}
-		else
-		{
-			m_ImageView->SetBackgroundColour("LIGHT GREY");
-		}
-
-		int64_t colorFG = options.GetAttributeInt(OName::ColorFG, -1);
-		if (colorFG != -1)
-		{
-			m_ImageView->SetForegroundColour(KxColor::FromCOLORREF(colorFG));
-		}
-		else
-		{
-			m_ImageView->SetForegroundColour("WHITE");
-		}
-		
-		// Description
-		m_Description = new KxHTMLWindow(m_Splitter, KxID_NONE);
-		AddUserWindow(m_Description);
-
-		// Split
-		m_Splitter->SetSashGravity(0);
-		m_Splitter->SetMinimumPaneSize(m_Description->GetCharHeight() * 3);
-
-		// Toolbar
-		m_ToolBar = new KxAuiToolBar(GetContentWindow(), KxID_NONE, KxAuiToolBar::DefaultStyle|wxAUI_TB_PLAIN_BACKGROUND);
-		m_ToolBar->AddStretchSpacer(1);
-
-		// BG color
-		m_ColorBGCtrl = new wxColourPickerCtrl(m_ToolBar, KxID_NONE, m_ImageView->GetBackgroundColour());
-		m_ColorBGCtrl->Bind(wxEVT_COLOURPICKER_CHANGED, &KImageViewerDialog::OnChangeColor, this);
-		m_ToolBar->AddControl(m_ColorBGCtrl);
-
-		// Backward
-		m_Backward = KMainWindow::CreateToolBarButton(m_ToolBar, KTr(KxID_BACKWARD), ImageResourceID::ControlLeft);
-
-		// Scale
-		m_ScaleSlider = new KxSlider(m_ToolBar, KxID_NONE, 100, 10, 500);
-		m_ScaleSlider->Bind(wxEVT_SLIDER, &KImageViewerDialog::OnScaleChanged, this);
-		m_ToolBar->AddControl(m_ScaleSlider);
-
-		// Forward
-		m_Forward = KMainWindow::CreateToolBarButton(m_ToolBar, KTr(KxID_FORWARD), ImageResourceID::ControlRight);
-		
-		// FG color
-		m_ColorFGCtrl = new wxColourPickerCtrl(m_ToolBar, KxID_NONE, m_ImageView->GetForegroundColour());
-		m_ColorFGCtrl->Bind(wxEVT_COLOURPICKER_CHANGED, &KImageViewerDialog::OnChangeColor, this);
-		m_ToolBar->AddControl(m_ColorFGCtrl);
-		
-		m_ToolBar->AddStretchSpacer(1);
-
-		// Realize
-		m_Backward->SetEnabled(false);
-		m_Forward->SetEnabled(false);
-
-		m_Backward->Bind(KxEVT_AUI_TOOLBAR_CLICK, &KImageViewerDialog::OnNavigation, this);
-		m_Forward->Bind(KxEVT_AUI_TOOLBAR_CLICK, &KImageViewerDialog::OnNavigation, this);
-		
-		m_ToolBar->Realize();
-
-		m_ToolBar->SetBackgroundColour(GetContentWindow()->GetBackgroundColour());
-		GetContentWindowSizer()->Add(m_ToolBar, 0, wxEXPAND);
-		AddUserWindow(m_ToolBar);
-		return true;
+		options.SetAttribute(OName::ColorBG, (int64_t)m_ImageView->GetBackgroundColour().GetPixel());
+		options.SetAttribute(OName::ColorFG, (int64_t)m_ImageView->GetForegroundColour().GetPixel());
 	}
-	return false;
-}
-KImageViewerDialog::KImageViewerDialog(wxWindow* parent, const wxString& caption)
-{
-	if (Create(parent, caption))
+	
+	bool KImageViewerDialog::Create(wxWindow* parent, const wxString& caption)
 	{
-		SetSize(KMainWindow::GetDialogBestSize(this));
-		GetButton(KxID_CLOSE).GetControl()->SetFocus();
-		CenterOnScreen();
+		if (KxStdDialog::Create(parent, KxID_NONE, KAux::StrOr(caption, KTr("ImageViewer.Caption")), wxDefaultPosition, wxDefaultSize, KxBTN_CLOSE))
+		{
+			AddButton(KxID_SAVE, wxEmptyString, true).GetControl()->Bind(wxEVT_BUTTON, &KImageViewerDialog::OnSaveImage, this);
+	
+			SetMainIcon(KxICON_NONE);
+			SetWindowResizeSide(wxBOTH);
+			SetInitialSize(parent->GetSize().Scale(0.7f, 0.7f));
+	
+			// Splitter
+			m_Splitter = new KxSplitterWindow(m_ContentPanel, KxID_NONE);
+			IThemeManager::GetActive().ProcessWindow(m_Splitter);
+			PostCreate(wxDefaultPosition);
+	
+			// View
+			m_ImageView = new KxImageView(m_Splitter, KxID_NONE);
+			m_ImageView->SetScaleMode(KxIV_SCALE_ASPECT_FIT);
+			m_ImageView->SetBackgroundMode(KxIV_BG_TRANSPARENCY_PATTERN);
+			m_ImageView->Bind(wxEVT_RIGHT_UP, [this](wxMouseEvent& event)
+			{
+				Close();
+			});
+			IThemeManager::GetActive().ProcessWindow(m_ImageView);
+	
+			// Options
+			using namespace Application;
+
+			auto options = IApplication::GetInstance()->GetGlobalOption(OName::ImageViewer);
+			int64_t colorBG = options.GetAttributeInt(OName::ColorBG, -1);
+			if (colorBG != -1)
+			{
+				m_ImageView->SetBackgroundColour(KxColor::FromCOLORREF(colorBG));
+			}
+			else
+			{
+				m_ImageView->SetBackgroundColour("LIGHT GREY");
+			}
+	
+			int64_t colorFG = options.GetAttributeInt(OName::ColorFG, -1);
+			if (colorFG != -1)
+			{
+				m_ImageView->SetForegroundColour(KxColor::FromCOLORREF(colorFG));
+			}
+			else
+			{
+				m_ImageView->SetForegroundColour("WHITE");
+			}
+			
+			// Description
+			m_Description = new KxHTMLWindow(m_Splitter, KxID_NONE);
+			AddUserWindow(m_Description);
+	
+			// Split
+			m_Splitter->SetSashGravity(0);
+			m_Splitter->SetMinimumPaneSize(m_Description->GetCharHeight() * 3);
+	
+			// Toolbar
+			m_ToolBar = new KxAuiToolBar(GetContentWindow(), KxID_NONE, KxAuiToolBar::DefaultStyle|wxAUI_TB_PLAIN_BACKGROUND);
+			m_ToolBar->AddStretchSpacer(1);
+	
+			// BG color
+			m_ColorBGCtrl = new wxColourPickerCtrl(m_ToolBar, KxID_NONE, m_ImageView->GetBackgroundColour());
+			m_ColorBGCtrl->Bind(wxEVT_COLOURPICKER_CHANGED, &KImageViewerDialog::OnChangeColor, this);
+			m_ToolBar->AddControl(m_ColorBGCtrl);
+	
+			// Backward
+			m_Backward = Utility::UI::CreateToolBarButton(m_ToolBar, KTr(KxID_BACKWARD), ImageResourceID::ControlLeft);
+			
+			// Scale
+			m_ScaleSlider = new KxSlider(m_ToolBar, KxID_NONE, 100, 10, 500);
+			m_ScaleSlider->Bind(wxEVT_SLIDER, &KImageViewerDialog::OnScaleChanged, this);
+			m_ToolBar->AddControl(m_ScaleSlider);
+	
+			// Forward
+			m_Forward = Utility::UI::CreateToolBarButton(m_ToolBar, KTr(KxID_FORWARD), ImageResourceID::ControlRight);
+			
+			// FG color
+			m_ColorFGCtrl = new wxColourPickerCtrl(m_ToolBar, KxID_NONE, m_ImageView->GetForegroundColour());
+			m_ColorFGCtrl->Bind(wxEVT_COLOURPICKER_CHANGED, &KImageViewerDialog::OnChangeColor, this);
+			m_ToolBar->AddControl(m_ColorFGCtrl);
+			
+			m_ToolBar->AddStretchSpacer(1);
+	
+			// Realize
+			m_Backward->SetEnabled(false);
+			m_Forward->SetEnabled(false);
+	
+			m_Backward->Bind(KxEVT_AUI_TOOLBAR_CLICK, &KImageViewerDialog::OnNavigation, this);
+			m_Forward->Bind(KxEVT_AUI_TOOLBAR_CLICK, &KImageViewerDialog::OnNavigation, this);
+			
+			m_ToolBar->Realize();
+	
+			m_ToolBar->SetBackgroundColour(GetContentWindow()->GetBackgroundColour());
+			GetContentWindowSizer()->Add(m_ToolBar, 0, wxEXPAND);
+			AddUserWindow(m_ToolBar);
+			return true;
+		}
+		return false;
 	}
-}
-KImageViewerDialog::~KImageViewerDialog()
-{
-	auto options = IApplication::GetInstance()->GetGlobalOption(OName::ImageViewer);
-	options.SetAttribute(OName::ColorBG, (int64_t)m_ImageView->GetBackgroundColour().GetPixel());
-	options.SetAttribute(OName::ColorFG, (int64_t)m_ImageView->GetForegroundColour().GetPixel());
 }
--- Kortex/Source/UI/KImageViewerDialog.h
+++ Kortex/Source/UI/KImageViewerDialog.h
@@ -6,157 +6,161 @@
 #include <KxFramework/KxSlider.h>
 #include <KxFramework/KxImageView.h>
 #include <KxFramework/KxHTMLWindow.h>
-#include "Archive/KArchive.h"
 #include <KxFramework/KxSplitterWindow.h>
+#include <Kx/EventSystem/Event.h>
+#include "Archive/KArchive.h"
 
-class KImageViewerEvent: public wxNotifyEvent
+namespace Kortex::UI
 {
-	public:
-		enum Type
-		{
-			FilePath,
-			BitmapPtr,
-			InputStream,
-		};
+	class KImageViewerEvent: public wxNotifyEvent
+	{
+		public:
+			enum Type
+			{
+				FilePath,
+				BitmapPtr,
+				InputStream,
+			};
+			
+		public:
+			KxEVENT_MEMBER(KImageViewerEvent, Next);
+			KxEVENT_MEMBER(KImageViewerEvent, Previous);
 
-	private:
-		std::variant<wxString, wxBitmap, const KArchive::Buffer*> m_Data;
-		bool m_HasPrev = false;
-		bool m_HasNext = false;
+		private:
+			std::variant<wxString, wxBitmap, const KArchive::Buffer*> m_Data;
+			bool m_HasPrev = false;
+			bool m_HasNext = false;
+			
+		private:
+			wxString GetString() const = delete;
+			void SetString(const wxString& s) = delete;
+			
+		public:
+			KImageViewerEvent() = default;
+			KImageViewerEvent(wxEventType type, const wxBitmap& bitmap)
+				:wxNotifyEvent(type)
+			{
+				SetBitmap(bitmap);
+			}
+			KImageViewerEvent(wxEventType type, const wxString& filePath)
+				:wxNotifyEvent(type)
+			{
+				SetFilePath(filePath);
+			}
+			KImageViewerEvent(wxEventType type, const KArchive::Buffer& buffer)
+				:wxNotifyEvent(type)
+			{
+				SetInputStream(buffer);
+			}
 
-	private:
-		wxString GetString() const = delete;
-		void SetString(const wxString& s) = delete;
+		public:
+			KImageViewerEvent* Clone() const override
+			{
+				return new KImageViewerEvent(*this);
+			}
+			Type GetType() const
+			{
+				return (Type)m_Data.index();
+			}
+			
+			bool HasPrev() const
+			{
+				return m_HasPrev;
+			}
+			bool HasNext() const
+			{
+				return m_HasNext;
+			}
+			void SetHasPrevNext(bool bPrev, bool bNext)
+			{
+				m_HasPrev = bPrev;
+				m_HasNext = bNext;
+			}
+			
+			wxString GetDescription() const
+			{
+				return wxNotifyEvent::GetString();
+			}
+			void SetDescription(const wxString& s)
+			{
+				wxNotifyEvent::SetString(s);
+			}
+			
+			bool HasBitmap() const;
+			wxBitmap GetBitmap() const;
+			void SetBitmap(const wxBitmap& bitmap);
+	
+			bool IsAnimationFile() const;
+			bool HasFilePath() const;
+			wxString GetFilePath() const;
+			void SetFilePath(const wxString& filePath);
+			
+			bool IsInputStream() const;
+			wxMemoryInputStream GetInputSteram();
+			void SetInputStream(const KArchive::Buffer& buffer);
+	};
+}
 
-	public:
-		KImageViewerEvent() {}
-		KImageViewerEvent(wxEventType type, const wxBitmap& bitmap)
-			:wxNotifyEvent(type)
-		{
-			SetBitmap(bitmap);
-		}
-		KImageViewerEvent(wxEventType type, const wxString& filePath)
-			:wxNotifyEvent(type)
-		{
-			SetFilePath(filePath);
-		}
-		KImageViewerEvent(wxEventType type, const KArchive::Buffer& buffer)
-			:wxNotifyEvent(type)
-		{
-			SetInputStream(buffer);
-		}
-		virtual ~KImageViewerEvent()
-		{
-		}
-
-		virtual KImageViewerEvent* Clone() const override
-		{
-			return new KImageViewerEvent(*this);
-		}
-
-	public:
-		Type GetType() const
-		{
-			return (Type)m_Data.index();
-		}
-
-		bool HasPrev() const
-		{
-			return m_HasPrev;
-		}
-		bool HasNext() const
-		{
-			return m_HasNext;
-		}
-		void SetHasPrevNext(bool bPrev, bool bNext)
-		{
-			m_HasPrev = bPrev;
-			m_HasNext = bNext;
-		}
-
-		wxString GetDescription() const
-		{
-			return wxNotifyEvent::GetString();
-		}
-		void SetDescription(const wxString& s)
-		{
-			wxNotifyEvent::SetString(s);
-		}
-
-		bool HasBitmap() const;
-		wxBitmap GetBitmap() const;
-		void SetBitmap(const wxBitmap& bitmap);
-
-		bool IsAnimationFile() const;
-		bool HasFilePath() const;
-		wxString GetFilePath() const;
-		void SetFilePath(const wxString& filePath);
-
-		bool IsInputStream() const;
-		wxMemoryInputStream GetInputSteram();
-		void SetInputStream(const KArchive::Buffer& buffer);
-};
-
-wxDECLARE_EVENT(KEVT_IMAGEVIEWER_NEXT_IMAGE, KImageViewerEvent);
-wxDECLARE_EVENT(KEVT_IMAGEVIEWER_PREV_IMAGE, KImageViewerEvent);
-
-class KImageViewerDialog: public KxStdDialog
+namespace Kortex::UI
 {
-	private:
-		KxSplitterWindow* m_Splitter = nullptr;
-		KxImageView* m_ImageView = nullptr;
-		KxHTMLWindow* m_Description = nullptr;
-		KxAuiToolBar* m_ToolBar = nullptr;
-		KxAuiToolBarItem* m_Backward = nullptr;
-		KxAuiToolBarItem* m_Forward = nullptr;
-		KxSlider* m_ScaleSlider = nullptr;
-		wxString m_FilePath;
-
-		wxColourPickerCtrl* m_ColorBGCtrl = nullptr;
-		wxColourPickerCtrl* m_ColorFGCtrl = nullptr;
-
-	private:
-		virtual int GetViewSizerProportion() const override
-		{
-			return 1;
-		}
-		virtual wxOrientation GetViewSizerOrientation() const override
-		{
-			return wxVERTICAL;
-		}
-		virtual wxOrientation GetViewLabelSizerOrientation() const override
-		{
-			return wxHORIZONTAL;
-		}
-		virtual bool IsEnterAllowed(wxKeyEvent& event, wxWindowID* id = nullptr) const override
-		{
-			return true;
-		}
-		virtual wxWindow* GetDialogMainCtrl() const override
-		{
-			return m_Splitter;
-		}
-		virtual void ResetState()
-		{
-			m_ImageView->SetBitmap(wxNullBitmap);
-		}
-		virtual bool OnDynamicBind(wxDynamicEventTableEntry& entry) override;
-
-		void OnLoadFromDisk(const wxString& filePath);
-		void OnNavigation(wxAuiToolBarEvent& event);
-		void OnAcceptNavigation(KImageViewerEvent& event);
-		void OnScaleChanged(wxCommandEvent& event);
-		void OnSaveImage(wxCommandEvent& event);
-		void OnChangeColor(wxColourPickerEvent& event);
-
-	public:
-		bool Create(wxWindow* parent, const wxString& caption = wxEmptyString);
-		KImageViewerDialog(wxWindow* parent, const wxString& caption = wxEmptyString);
-		virtual ~KImageViewerDialog();
-
-	public:
-		void Navigate(KImageViewerEvent& event)
-		{
-			OnAcceptNavigation(event);
-		}
-};
+	class KImageViewerDialog: public KxStdDialog
+	{
+		private:
+			KxSplitterWindow* m_Splitter = nullptr;
+			KxImageView* m_ImageView = nullptr;
+			KxHTMLWindow* m_Description = nullptr;
+			KxAuiToolBar* m_ToolBar = nullptr;
+			KxAuiToolBarItem* m_Backward = nullptr;
+			KxAuiToolBarItem* m_Forward = nullptr;
+			KxSlider* m_ScaleSlider = nullptr;
+			wxString m_FilePath;
+	
+			wxColourPickerCtrl* m_ColorBGCtrl = nullptr;
+			wxColourPickerCtrl* m_ColorFGCtrl = nullptr;
+	
+		private:
+			int GetViewSizerProportion() const override
+			{
+				return 1;
+			}
+			wxOrientation GetViewSizerOrientation() const override
+			{
+				return wxVERTICAL;
+			}
+			wxOrientation GetViewLabelSizerOrientation() const override
+			{
+				return wxHORIZONTAL;
+			}
+			bool IsEnterAllowed(wxKeyEvent& event, wxWindowID* id = nullptr) const override
+			{
+				return true;
+			}
+			wxWindow* GetDialogMainCtrl() const override
+			{
+				return m_Splitter;
+			}
+			void ResetState()
+			{
+				m_ImageView->SetBitmap(wxNullBitmap);
+			}
+			bool OnDynamicBind(wxDynamicEventTableEntry& entry) override;
+	
+			void OnLoadFromDisk(const wxString& filePath);
+			void OnNavigation(wxAuiToolBarEvent& event);
+			void OnAcceptNavigation(KImageViewerEvent& event);
+			void OnScaleChanged(wxCommandEvent& event);
+			void OnSaveImage(wxCommandEvent& event);
+			void OnChangeColor(wxColourPickerEvent& event);
+	
+		public:
+			KImageViewerDialog(wxWindow* parent, const wxString& caption = wxEmptyString);
+			~KImageViewerDialog();
+	
+		public:
+			bool Create(wxWindow* parent, const wxString& caption = wxEmptyString);
+			void Navigate(KImageViewerEvent& event)
+			{
+				OnAcceptNavigation(event);
+			}
+	};
+}

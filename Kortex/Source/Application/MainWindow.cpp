ddb7e811a42f5f0be77a78728ae64b4abeab9eba
Kerber <DaIvan97@gmail.com>
2019-11-03 08:08:48 +0300

    Added support for 'KxStatusBarEx' built-in progress bar

--- Kortex/Source/Application/MainWindow.cpp
+++ Kortex/Source/Application/MainWindow.cpp
@@ -391,22 +391,20 @@ namespace Kortex::Application
 
 	void MainWindow::ClearStatus(int index)
 	{
-		wxWindowUpdateLocker lock(m_StatusBar);
-
 		m_StatusBar->SetStatusText(wxEmptyString, index + 1);
 		m_StatusBar->SetStatusImage(-1, index + 1);
 	}
 	void MainWindow::SetStatus(const wxString& label, int index, const ResourceID& image)
 	{
-		wxWindowUpdateLocker lock(m_StatusBar);
-
 		m_StatusBar->SetStatusText(label, index + 1);
 		m_StatusBar->SetStatusImage(image.AsInt(), index + 1);
 	}
 	void MainWindow::SetStatusProgress(int current)
 	{
+		m_StatusBar->SetValue(current);
 	}
 	void MainWindow::SetStatusProgress(int64_t current, int64_t total)
 	{
+		m_StatusBar->SetValue(current, total);
 	}
 }

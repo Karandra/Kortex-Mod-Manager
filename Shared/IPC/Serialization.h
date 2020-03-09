7acbaa343e443a507b3d6e2a151ef52275889f40
Kerber <DaIvan97@gmail.com>
2020-03-10 00:52:06 +0300

    IPC Serializer code cleanup

--- Shared/IPC/Serialization.h
+++ Shared/IPC/Serialization.h
@@ -4,7 +4,7 @@
 
 namespace Kortex::IPC
 {
-	class Serializer
+	class Serializer final
 	{
 		private:
 			template<class Head, class... Tail>
@@ -14,24 +14,29 @@ namespace Kortex::IPC
 				(void)std::initializer_list<int> {((serializer.PushValue(tail)), 0)...};
 			}
 
-			template<class... Args> struct ConvertToTuple
+			template<class... Args>
+			struct ConvertToTuple
 			{
-				template<size_t... t_Sequence> std::tuple<Args...> operator()(const Serializer& serializer, std::index_sequence<t_Sequence...>)
+				template<size_t... t_Sequence>
+				std::tuple<Args...> operator()(const Serializer& serializer, std::index_sequence<t_Sequence...>)
 				{
 					return std::make_tuple(Args {serializer.Get<Args>(t_Sequence)}...);
 				}
 			};
 
 		public:
-			template<class... Args> static wxString Serialize(Args&&... arg)
+			template<class... Args>
+			static wxString Serialize(Args&&... arg)
 			{
 				Serializer serializer;
 				SerializeAux(serializer, arg...);
 				return serializer.ToString();
 			}
-			template<class... Args> static std::tuple<Args...> Deserialize(const wxString& serializedData)
+			
+			template<class... Args>
+			static std::tuple<Args...> Deserialize(const wxString& serialized)
 			{
-				Serializer serializer(serializedData);
+				Serializer serializer(serialized);
 				return ConvertToTuple<Args...>()(serializer, std::make_index_sequence<sizeof...(Args)>());
 			}
 
@@ -39,14 +44,14 @@ namespace Kortex::IPC
 			KxXMLDocument m_XML;
 			KxXMLNode m_RootNode;
 
-			const wxString m_EntryName = wxS("Entry");
+			const wxString m_ItemName = wxS("Item");
 			const wxString m_RootName = wxS("Serialized");
 
 		private:
 			KxXMLNode GetNthNode(const size_t index) const
 			{
 				size_t i = 0;
-				for (KxXMLNode node = m_RootNode.GetFirstChildElement(); node.IsOK(); node = node.GetNextSiblingElement())
+				for (KxXMLNode node = m_RootNode.GetFirstChildElement(); node; node = node.GetNextSiblingElement())
 				{
 					if (i == index)
 					{
@@ -54,7 +59,7 @@ namespace Kortex::IPC
 					}
 					i++;
 				}
-				return KxXMLNode();
+				return {};
 			}
 
 		public:
@@ -62,8 +67,8 @@ namespace Kortex::IPC
 			{
 				m_RootNode = m_XML.NewElement(m_RootName);
 			}
-			Serializer(const wxString& serializedData)
-				:m_XML(serializedData)
+			Serializer(const wxString& serialized)
+				:m_XML(serialized)
 			{
 				m_RootNode = m_XML.ConstructElement(m_RootName);
 			}
@@ -75,22 +80,53 @@ namespace Kortex::IPC
 			}
 
 		public:
-			template<class T> void PushValue(const T& value)
+			template<class T>
+			void PushValue(T&& value)
 			{
-				static_assert(std::is_integral_v<T> || std::is_enum_v<T>);
+				using Tx = std::remove_reference_t<std::remove_cv_t<T>>;
 
-				m_RootNode.NewElement(m_EntryName).SetValue(static_cast<int64_t>(value));
+				if constexpr (std::is_integral_v<Tx> || std::is_enum_v<Tx>)
+				{
+					m_RootNode.NewElement(m_ItemName).SetValue(static_cast<int64_t>(value));
+				}
+				else if constexpr (std::is_floating_point_v<Tx>)
+				{
+					m_RootNode.NewElement(m_ItemName).SetValue(static_cast<double>(value));
+				}
+				else
+				{
+					static_assert(false, "unsupported data type");
+				}
 			}
-			template<> void PushValue(const wxString& value)
+			
+			template<>
+			void PushValue(const wxString& value)
 			{
-				m_RootNode.NewElement(m_EntryName).SetValue(value);
+				m_RootNode.NewElement(m_ItemName).SetValue(value);
 			}
 	
-			template<class T> T Get(size_t index, T defaultValue = {}) const
+			template<class T>
+			T Get(size_t index, const T& defaultValue = {}) const
 			{
-				return GetNthNode(index).GetValueInt(defaultValue);
+				using Tx = std::remove_reference_t<std::remove_cv_t<T>>;
+
+				if constexpr (std::is_integral_v<Tx> || std::is_enum_v<Tx>)
+				{
+					return static_cast<Tx>(GetNthNode(index).GetValueInt(defaultValue));
+				}
+				else if constexpr (std::is_floating_point_v<Tx>)
+				{
+					return static_cast<Tx>(GetNthNode(index).GetValueFloat(defaultValue));
+				}
+				else
+				{
+					static_assert(false, "unsupported data type");
+				}
+				return {};
 			}
-			template<> wxString Get(size_t index, wxString defaultValue) const
+			
+			template<>
+			wxString Get(size_t index, const wxString& defaultValue) const
 			{
 				return GetNthNode(index).GetValue(defaultValue);
 			}
